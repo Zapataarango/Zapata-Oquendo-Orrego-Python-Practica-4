d7a8b87fc96f66292d42974a850f2b58909d6358
felzap <felzap@bancolombia.com.co>
2026-06-01 16:00:34 -0500

    se añade imagen de frontend docker composer yml

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api import auth, usuarios, reservas, espacios
 from app.db import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -8,6 +9,17 @@ app = FastAPI(
     title="Sistema de Reservas",
     version="1.0.0",
     swagger_ui_parameters={"defaultModelsExpandDepth": -1})
+
+# Configuración de CORS para permitir solicitudes desde el frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Registrar routers
 app.include_router(auth.router)
