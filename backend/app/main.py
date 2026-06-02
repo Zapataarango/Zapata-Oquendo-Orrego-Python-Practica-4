8f73aa1fcac8c4a6f9b4f6cff0bbf74b29bb5626
Zapataarango <zapataarangofelipe@gmail.com>
2026-06-02 10:41:13 -0500

    Se ajusta conexión de back con front

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api import auth, usuarios, reservas, espacios
 from app.db import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -8,6 +10,16 @@ app = FastAPI(
     title="Sistema de Reservas",
     version="1.0.0",
     swagger_ui_parameters={"defaultModelsExpandDepth": -1})
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Registrar routers
 app.include_router(auth.router)
